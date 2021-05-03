bb3045797334bc7d97976094350725bbf0054477
toledanogus <toledanogu@gmail.com>
2021-05-03 13:15:50 -0500

    Con Babel y todo funcionando ok

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,18 +2,27 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+
 
 module.exports = {
 
     mode: 'production',
     optimization: {
-        minimizer: [ new OptimizeCssAssetsPlugin()]
+        minimizer: [new OptimizeCssAssetsPlugin()],
     },
     output: {
         filename: 'main.[contentHash].js'
     },
     module: {
         rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            },
             {
                 test: /\.css$/,
                 exclude: /styles\.css$/,
@@ -71,6 +80,7 @@ module.exports = {
         new CopyPlugin([
             {from: 'src/assets', to: 'assets/'}
         ]),
+        new MinifyPlugin()
     ]
 
 }
